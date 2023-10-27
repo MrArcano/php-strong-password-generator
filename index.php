40146f4e0eceda438bcdca96049cdbb221cff095
MrArcano <salvatore-dario@hotmail.it>
2023-10-27 15:26:44 +0200

    [edit]: layout

--- index.php
+++ index.php
@@ -17,8 +17,26 @@
     <div class="box-generator d-flex flex-column align-items-center justify-content-center ">
       <h1>Strong Password Generator</h1>
       <h2>Genera una password sicura</h2>
-      <div class="error">
+      <div class="error my-3">
         <p>Scegliere una password con un minimo di 8 caratteri e un massimo di 32 caratteri</p>
+      </div>
+      <div class="form-container">
+        <form action="index.php" method="post">
+          <div class="row mb-3">
+            <div class="col-auto d-flex align-items-center ">
+              <label class="form-label m-0" for="num-pass">Lunghezza password:</label>
+            </div>
+            <div class="col-auto">
+              <input class="form-control" type="number" name="numPassword" id="num-pass">
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="reset" class="btn btn-secondary">Annulla</button>
+          </div>
+          
+        </form>
       </div>
     </div>
   </div>
