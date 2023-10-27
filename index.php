0ce503622f941f9122302cb796e0b52923151f5f
MrArcano <salvatore-dario@hotmail.it>
2023-10-27 15:07:26 +0200

    [init]

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  <!-- Custom css -->
+  <link rel="stylesheet" href="./css/style.css">
+
+  <title>PHP Strong Password Generator</title>
+</head>
+<body>
+  <div class="container">
+    <div class="box-generator d-flex flex-column align-items-center justify-content-center ">
+      <h1>Strong Password Generator</h1>
+      <h2>Genera una password sicura</h2>
+      <div class="error">
+        <p>Scegliere una password con un minimo di 8 caratteri e un massimo di 32 caratteri</p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
